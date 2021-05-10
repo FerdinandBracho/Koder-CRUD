62808ce58df9ecdc1058f3ab16401974db2063d4
Ferdinand Bracho <ferdinand.bracho@gmail.com>
2021-05-10 18:47:18 -0500

    Optional parameter in function js

--- js/base.js
+++ js/base.js
@@ -1,4 +1,5 @@
 //Function with optional paremeter 
+////////////////////////////////////////////////////////////////
 
 // Valid since ES2015
 function sumTwonum(numA, numB = 1)  {
@@ -17,3 +18,14 @@ function sumTwonum2(numA, numB)  {
 }
 
 console.log(sumTwonum2(5))
+////////////////////////////////////////////////////////////
+
+//multiples optional parameters 
+
+function sumFour(numa, numb, numc, numd)    {
+    numc = numc || 1
+    numd = numd || 1
+    return numa + numb + numc + numd
+}
+
+console.log(sumFour(1, 1, 2))
