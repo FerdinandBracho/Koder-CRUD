1deea2f21444ed174345290b55b92840a677547a
Ferdinand Bracho <ferdinand.bracho@gmail.com>
2021-05-14 14:55:14 -0500

    Practicas clase 8

--- js/base.js
+++ js/base.js
@@ -1,46 +1,36 @@
-// Ejercicio 1 .forEach()
-// Dado un array con solo números
-// obtener el valor mas alto
-// input: [1,4,3,2,5]
-// output: -> 5
+//! Ejercicio en clase 1
+const stringItUp = arr => arr.map((value) => value.toString())
 
-// let arrExOne = [1,2,3,4,5,99]
-// let greater = arrExOne[0]
+let arrayToTest = [2,5,100]
+console.log(stringItUp(arrayToTest))
 
-// arrExOne.forEach((value) => value > greater ? greater = value: '')
-// console.log(greater)
+//! Ejercicio en clase 2
+const capitalizeNames = arr => arr.map((value) => value[0].toUpperCase() + value.substring(1).toLowerCase())
+ 
+const arrTT = ["Jorge", "AntoniO", "JUan", "Victor"]
+console.log(capitalizeNames(arrTT))
 
-///////////////////////////////////////////////////////////////////////
-// ejercicio 2 .forEach() o .map() o for()
-// función
-// convertir todos los elementos numericos de un array
-// a strings
-// input: [1,2,3,4,5]
-// output -> ['1,','2','3','4','5']
+//! Ejercicio en clase 3
+const joinNames = (arr, arrt) => arr.map((value, index) => value + " " + arrt[index])
 
-// let arrExTwo = [1,2,3,4,5]
-// let arrExTwoString = []
+let arrOne = ['jorge', 'luis']
+let arrTwo = ['camarillo','cristobal']
+console.log(joinNames(arrOne, arrTwo))
 
-// arrExTwo.forEach((value) => arrExTwoString += value.toString())
+//! Ejercicio en clase 4 
+const stringCon = arr => arr.reduce((result, value) => result += value, '')
 
-// console.log(arrExTwoString)
+let arrThree = [1,2,3]
+console.log(`The result of the reduce operation is: ${stringCon(arrThree)}`)
 
-///////////////////////////////////////////////////////////////////////
-// Ejercicio 3 .funcion que dados 2 parametros .forEach() o .map() o for()
-// p1 = año de inicio
-// p2 = año de fin
-// y devuelva un array con los años que son bisiestos
-// que se encuentran en ese rango
-// [1990, 2000, 2001, 2020]
-// -> [2000,2020]
+//! Ejercicio en clase 5
+const totalShoppingCart = arr => arr.reduce((result, value, index) => result += (value[1]), 0)
 
-// let yInit = 2000
-// const yFin = 2020
-// let resultingYears = []
-
-
-// Si el año es uniformemente divisible por 4, vaya al paso 2. De lo contrario, vaya al paso 5.
-// Si el año es uniformemente divisible por 100, vaya al paso 3. De lo contrario, vaya al paso 4.
-// Si el año es uniformemente divisible por 400, vaya al paso 4. De lo contrario, vaya al paso 5.
-// El año es un año bisiesto (tiene 366 días).
-// El año no es un año bisiesto (tiene 365 días).
+let arrFive = 
+    [ 
+        ["Reloj", 500] , 
+        ["Reloj", 300] , 
+        ["Reloj", 1200],
+        ["Reloj", 5000]
+    ] 
+console.log(`El total es de: $${totalShoppingCart(arrFive)},00`)
---
+++ js/practices-class-08.js
@@ -0,0 +1,36 @@
+// ! Practica #1 - Contabilizar votantes
+const totalVoters = arr => arr.reduce((total, value) => total += value[1], 0)
+
+// !Declaracion, ejecuscion y presentacion de consola de funcion
+let arrOne = [ ["jorge", false] , ["luis", true] , ["Victor", true],['ferdinand', true], ['Rosy', true]]
+console.log(totalVoters(arrOne ))
+
+///////////////////////////////////////////////////
+
+// ! Practica #2 - Funciones varias aplicables a pedidos
+
+// ! Declaracion de funciones 
+const totalPaella = arr => arr.reduce((total, value) => total+= value[1] === 'paella' ? 1 : 0 , 0)
+const postresPlatanoHelado = arr => arr.filter((value, index) => value[2] === 'platano' || value[2] === 'helado')
+const costoSuperior = arr => arr.filter((value) => value[3] > 90)
+const costoMenor = arr => arr.filter((value) => value[3] <= 90)
+
+// !Array de pedidos
+let pedidos = 
+    [
+        ['ensalada de pepinos','paella','platano', 100],
+        ['ensalada de tomates','pescado','helado', 120],
+        ['ensalada simple','paella','yogurt', 80],
+        ['ensalada simple','enchiladas','yogurt', 80],
+        ['ensalada cesar','salmón','platano', 100],
+    ]
+
+// ! Ejecucion de funciones y presentacion de resultados en consola
+console.log(`Total de paellas pedidas: ${totalPaella(pedidos)}`)
+console.log('Pedidos con postres selecionados: ', postresPlatanoHelado(pedidos))
+console.log('Pedidos con costo superior a 90$: ', costoSuperior(pedidos))
+console.log('Pedidos con costo menor a 90$: ', costoMenor(pedidos))
+//////////////////////////////////////////////////
+/**
+ * Estudiar el tema de Objetos y métodos de Objetos
+ */
