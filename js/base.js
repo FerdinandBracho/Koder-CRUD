184563fb0720fc33284d8f73f102604a845256f4
Ferdinand Bracho <ferdinand.bracho@gmail.com>
2021-05-21 14:43:15 -0500

    sesion 12 con practica

--- js/base.js
+++ js/base.js
@@ -1,77 +1,137 @@
-// ! Tabla arrays 
-let tablaKoder = document.querySelector(".tbody-array")
-let koders = ['antonio', 'victor', 'rose', 'Ferdinand','Jorge']
-let listKoder = ''
-
-koders.forEach((koderName, index) =>{
-    listKoder += `
-        <tr>
-            <td> ${index} </td>
-            <td> ${koderName} </td>
-        </tr>
-    `
-})
-
-console.log(listKoder)
-tablaKoder.innerHTML = listKoder
-
-// !Tabla Objetos 
-
-let tablaKoderO = document.querySelector(".tbody-obj")
-let kodersObject  = [
+let koders = [
     {
-        name: 'Ferdinand',
-        lastName: 'bracho',
-        age: 30,
-        github:'@Ferdinand-Bracho'
-    },{
-        name:'Ferdinand',
-        lastName:'bracho',
-        age: 30,
-        github:'@Ferdinand-Bracho'
-    },{
-        name:'Ferdinand',
-        lastName:'bracho',
-        age: 30,
-        github:'@Ferdinand-Bracho'
-    },{
-        name: 'Ferdinand',
-        lastName: 'bracho',
-        age: 30,
-        github:'@Ferdinand-Bracho'
-    },{
-        name:'Ferdinand',
-        lastName:'bracho',
-        age: 30,
-        github:'@Ferdinand-Bracho'
+        id: 23,
+        name:'jorge'
+    },
+    {
+        id: 31,
+        name:'Antonio'
+    },
+    {
+        id: 212,
+        name:'fer'
+    },
+    {
+        id: 20,
+        name:'rose'
     }
+
 ]
 
-let listKoderO = ''
-kodersObject.forEach((item, index) => {
-    let {name, lastName, age, github} = item
-    listKoderO += `
-        <tr>
-            <td> ${index} </td>
-            <td> ${name}</td>
-            <td> ${lastName}</td>
-            <td> ${age}</td>
-            <td> ${github}</td>
-        </tr>
-    `
+
+let listKoders = [...koders]
+let enrolledKoders = [{
+    id: 234,
+    name:'Yair'
+}]
+
+// // print table koders
+const printTableKoders = (arrKoders, selector, type) => {
+    // temp
+    let accKodersList = ''
+    arrKoders.forEach(( koder, index) => {
+        if(type === 'list'){
+            accKodersList += `
+            <tr class="trkoder_${koder.id}">
+                <td>${koder.id}</td>
+                <td>${koder.name}</td>
+                <td>
+                    <button class="btn btn-primary enrolledKoderButton" data-id="${koder.id}" data-index="${index}">Inscribir</button>
+                </td>
+            </tr>
+            `
+        } else {
+            accKodersList += `
+            <tr class="trkoder_${koder.id}">
+                <td>${koder.id}</td>
+                <td>${koder.name}</td>
+                <td>
+                    <button class="btn btn-warning removeKoderButton" data-id="${koder.id}" data-index="${index}">Dar de baja</button>
+                </td>
+            </tr>
+            `
+        }   
+    })
+    // DOM
+    let sel = document.querySelector(selector)
+    sel.innerHTML = accKodersList
+} 
+
+const updateTable = () => {
+    printTableKoders(listKoders, '.table__koders tbody', 'list')
+    printTableKoders(enrolledKoders, '.table__kodersEnrrolled tbody', 'enrolled')
+}
+
+let wrappKoderList = document.querySelector('.table__koders tbody')
+wrappKoderList.addEventListener('click', (event) => {
+    if( event.target.classList.contains('enrolledKoderButton') ) {
+
+        let idKoder = parseInt(event.target.dataset.id)
+
+        let koderItem = listKoders.filter(koder => koder.id === idKoder )
+        enrolledKoders.push(koderItem[0])
+
+        let kodersNotDeleted = listKoders.filter(koder => koder.id !== idKoder)
+        listKoders = [...kodersNotDeleted]
+
+        updateTable()
+    }
 })
 
-tablaKoderO.innerHTML = listKoderO
+let wrappKoderEnrrolled = document.querySelector('.table__kodersEnrrolled tbody')
+wrappKoderEnrrolled.addEventListener('click', (element) => {
+    console.log(element.target.classList)
+    if(element.target.classList.contains('removeKoderButton')) {
+        let idKoder = parseInt(element.target.dataset.id)
+        
+        let enrrolledKoder = enrolledKoders.filter( koder => koder.id == idKoder )
+        let newdataEnrrolledKoder = enrolledKoders.filter( koder => koder.id !== idKoder )
+
+        listKoders.push(enrrolledKoder[0])
+        enrolledKoders = [...newdataEnrrolledKoder]
+        
+        updateTable()
+    }
+})
 
 
-// koders.forEach((koderName, index) =>{
-//     listKoder += `
-//         <tr>
-//             <td> ${index} </td>
-//             <td> ${koderName} </td>
-//         </tr>
-//     `
-// })
 
-// console.log(listKoder)
-// tablaKoder.innerHTML = listKoder
+btnMostrar= document.querySelector('.btn-mostrar')
+
+btnMostrar.addEventListener('click', (event) => {
+    updateTable()
+})
+
+btnReset = document.querySelector('.btn-reset')
+
+btnReset.addEventListener('click', (event) => {
+        // listKoders = [...listKoders, ...enrolledKoders]
+        listKoders = listKoders.concat(enrolledKoders)
+        enrolledKoders = []
+        updateTable()
+})
+ 
+introKoders = document.getElementById('introKodersForm')
+
+introKoders.addEventListener('submit', (event) => {
+    event.preventDefault()
+    if (document.querySelector('.idK').value === '' || 
+        document.querySelector('.nameK').value === '') {
+            alert('Ingresa los valores correspondientes en los campos para el alta')
+        }
+
+    // else if (typeof document.querySelector('.idK').value != "number"){
+    //     alert('El Id del Koder debe ser un vaor numerico')
+    // }
+
+    else {
+        let newKoder = {
+            id: parseInt(document.querySelector('.idK').value),
+            name: document.querySelector('.nameK').value
+        }
+        listKoders.push(newKoder)
+        updateTable()
+        document.querySelector('.idK').value = '' 
+        document.querySelector('.nameK').value = ''
+    }
+})
