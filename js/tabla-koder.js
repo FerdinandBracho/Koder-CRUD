7df11787500f3c4b31a6374088973bb16cf35d24
Ferdinand Bracho <ferdinand.bracho@gmail.com>
2021-05-26 20:07:59 -0500

    clase 16 practica

--- js/tabla-koder.js
+++ js/tabla-koder.js
@@ -1,44 +1,115 @@
-function printTable() {
-    
-    let tableKoder = document.querySelector('.t-koder tbody')
+// !Pintar Tabla en pantalla principal 
+let tableKoder = document.querySelector('.t-koder tbody')
 
-    const tableRequest = new XMLHttpRequest
-
-    tableRequest.addEventListener('readystatechange', () => {
-        if (tableRequest.readyState != 4) {
-            return
-        }
-        else {
-            if (tableRequest.readyState === 4 &&
-                tableRequest.status >= 200 &&
-                tableRequest.status <= 299) {
-                    const response = tableRequest
-                    const objectResponse = JSON.parse(response.responseText)                    
-                    let postTableKoders = ''
-
-                    for (item in objectResponse) {
-                        postTableKoders += `
-                        <tr>
-                            <td>${objectResponse[item].name}</td>
-                            <td>${objectResponse[item].lastName}</td>
-                            <td>${parseInt(objectResponse[item].age)}</td>
-                            <td>${objectResponse[item].position}</td>
-                            <td><a href="index.html?hashkoder=${item}" ><button onclick="deleteKoder()" class="btn btn-danger" id="btn-delete-koder">Eliminar</button></a></td>
-                            <td><a href="koder.html?hashkoder=${item}" ><button class="btn btn-secondary" id="btn-delete-koder">Ver/Editar</button></a></td>
-                        </tr>                   
-                        `                           
-                    } 
-
-                    tableKoder.innerHTML = postTableKoders
-                }
-            else {
-                console.warn('No se pudo ejecutar')
-            }
-        }
-    })
-
-    tableRequest.open('GET', 'https://sessiones-js-default-rtdb.firebaseio.com/koders/.json')
-    tableRequest.send()
+// !Funcion para pintar tabla (toma un arry como argumento al ejecutarse)
+const printTable = objectKoder => {
+    let postTableKoders = ''
+    for (item in objectKoder) {
+        let koder = objectKoder[item]
+        postTableKoders += `
+        <tr>
+            <td>${koder.name}</td>
+            <td>${koder.lastName}</td>
+            <td>${parseInt(koder.age)}</td>
+            <td>${koder.position}</td>
+            <td><a href="koder.html?hashkoder=${item}" ><button class="btn btn-secondary" id="btn-delete-koder">Ver/Editar</button></a></td>
+            <td><button class="btn btn-danger btn-delete-koder" data-id="${item}" >Eliminar</button></td>
+        </tr>                   
+        `                           
+    } 
+    document.querySelector('.t-koder tbody').innerHTML = postTableKoders
 }
 
-printTable()
+// !Funcion para Fetch y ejecucion de funcion printTable con response como argumento
+const printTableFinal = () => {
+    fetch('https://sessiones-js-default-rtdb.firebaseio.com/koders/.json')
+    .then(result => {
+        return result.json()
+    }).then(response => {
+    printTable(response)
+    }).catch((error) => {
+        console.error('Error:', error);
+      });
+}
+
+
+document.querySelector('body').addEventListener("pageshow", printTableFinal())
+
+// ? //////////////////////////////////////////////////////////////////////////////////////
+
+// ! Alta de Nuevo Koder 
+let formAltaBtn = document.getElementById('btn-form-alta')
+
+// !Funcion para limpiarlos campos inputs
+const cleanInput = () => {
+    inputName.value = ''
+    inputLastName.value = ''
+    inputAge.value = ''
+    inputPosition.value = ''
+}
+
+// !Evento para accionar el alta 
+formAltaBtn.addEventListener('click', () => {
+    // ! Seteo datos
+    let inputName = document.getElementById('inputName')
+    let inputLastName = document.getElementById('inputLastName')
+    let inputAge = document.getElementById('inputAge')
+    let inputPosition = document.getElementById('inputPosition')
+
+    // !Validacion de compos
+    if (inputName.value === '' || 
+        inputLastName.value === '' || 
+        inputAge.value === '' || 
+        inputPosition.value === '') {
+
+        alert('Completa los campos obligatorios para Alta de Koder')
+        cleanInput()
+        return
+    }
+    
+    // !Seteo nuevo koder obj
+    let newKoder = {
+        name: inputName.value,
+        lastName: inputLastName.value,
+        age: parseInt(inputAge.value),
+        position: inputPosition.value
+    }
+
+    // !Fetch Post a la db del nuevo koder
+    fetch('https://sessiones-js-default-rtdb.firebaseio.com/koders/.json', {
+        method: 'POST',
+        body: JSON.stringify(newKoder)
+    }).then(res => {
+        cleanInput()
+        printTableFinal()
+        return res.json()
+    }).then(response => {
+        console.log(response)
+    }).catch((error) => {
+        console.error('Error:', error);
+      });
+
+})
+
+// ? //////////////////////////////////////////////////////////////////////////////////////
+
+// ! Eliminar Koder seleccionado
+tableKoder.addEventListener('click', (e) => {
+    if (e.target.classList.contains('btn-delete-koder')) {
+        let idkoder = e.target.dataset.id
+
+        // !Fetch delete koder seleccionado
+        fetch(`https://sessiones-js-default-rtdb.firebaseio.com/koders/${idkoder}/.json`, {
+            method: 'DELETE'
+        }).then(res => {
+            res.json()
+            console.log(`Koder ${idkoder} eliminado con exito`)
+            return printTableFinal()
+        }).catch((error) => {
+            console.error('Error:', error);
+          });
+
+    }           
+})
+
+// ? //////////////////////////////////////////////////////////////////////////////////////
--- js/ver-editar.js
+++ js/ver-editar.js
@@ -1,87 +1,59 @@
-let search = window.location.search
-let hashKoder = search.slice(search.indexOf('=') + 1)
+// !Extrayendo query param 
+const params = new URLSearchParams(window.location.search)
+let hashKoder = params.get('hashkoder')
 
-const singleKoder = new XMLHttpRequest
-singleKoder.open('GET', `https://sessiones-js-default-rtdb.firebaseio.com/koders/${hashKoder}/.json`)
+// ? //////////////////////////////////////////////////////////////////////////////
 
-singleKoder.addEventListener('readystatechange', () => {
-    if (singleKoder.readyState !== 4) {
-        return
-    }
-    else {
-        if (singleKoder.status >= 200 && singleKoder.status <= 299) {
-                const response = singleKoder
-                const  objectResponse = JSON.parse(response.responseText)
-                console.log(objectResponse)
-                let singleKoderData = ''
+// ! Funcion con Fetch para jalar data del koder seleccionado y mostrarla
+const printSingleKoder = () => {
+    fetch(`https://sessiones-js-default-rtdb.firebaseio.com/koders/${hashKoder}/.json`)
+    .then(res => {
+        return res.json()
+    }).then(response => {
+        let {name, lastName, age, position} = response
+        document.getElementById('inputNameEdit').setAttribute('value', name)
+        document.getElementById('inputLastNameEdit').setAttribute('value', lastName)
+        document.getElementById('inputAgeEdit').setAttribute('value', age)
+        document.getElementById('inputPositionEdit').setAttribute('value', position)
 
-                for (data in objectResponse) {
-                    let = {name, lastName , age, position} = objectResponse
-                    
-                    document.getElementById('inputNameE').setAttribute('placeholder',`Nombre: ${name}` )
-                    document.getElementById('inputLastNameE').setAttribute('placeholder', `Apellido: ${lastName}`)
-                    document.getElementById('inputAgeE').setAttribute('placeholder', `Edad: ${age}`)
-                    document.getElementById('inputPositionE').setAttribute('placeholder', `Rol: ${position}`)
-
-                    singleKoderData = `
-                        <h3>${name} ${lastName}</h3>
-                        <p>Tiene ${age} años de edad</p>
-                        <p>Cumple el rol de ${position} dentro de Kodemia</p>
-                    `
-                }
-                let cardBody = document.querySelector('.cbody')
-                cardBody.innerHTML = singleKoderData
-            }
-        else {
-            console.warn('No se pudo ejecutar')
+        let singleKoderData = ''
+        for (dta in response) {
+            singleKoderData = `
+                <h3>${name} ${lastName}</h3>
+                <p>Tiene ${age} años de edad</p>
+                <p>Cumple el rol de ${position} dentro de Kodemia</p>   
+    `
         }
-    }
-})
+        document.querySelector('.cbody').innerHTML = singleKoderData
+    }).catch((error) => {
+        console.error('Error:', error);
+      });
+}
 
-singleKoder.send()
+printSingleKoder()
 
+// ? //////////////////////////////////////////////////////////////////////////////
 
 // !Edit Koder properties 
 
-const editKoder = new XMLHttpRequest
-editKoder.open('POTCH', `https://sessiones-js-default-rtdb.firebaseio.com/koders/${hashKoder}/.json`)
+let btnEdit = document.getElementById('btn-form-edit')
 
-editKoder.addEventListener('readystatechange', () => {
-    if (editKoder.readyState !== 4) {
-        return
+btnEdit.addEventListener('click', () => {
+    let dataToPut = {
+        name: document.getElementById('inputNameEdit').value,
+        lastName: document.getElementById('inputLastNameEdit').value,
+        age: document.getElementById('inputAgeEdit').value,
+        position: document.getElementById('inputPositionEdit').value
     }
-    else {
-        if (editKoder.status >= 200 && editKoder.status <= 299) {
-                const response = editKoder
-                const  objectResponse = JSON.parse(response.responseText)
-                console.log(objectResponse)
-                let dataToPocht = {
-                    name: '',
-                    lastName: '',
-                    age: '',
-                    position: ''
-                }
-
-                let btnEdit = document.getElementById('btn-form-edit')
-                btnEdit.addEventListener('click', () => {
-
-                    dataToPocht.name = document.getElementById('inputNameE').value
-                    dataToPocht.lastName = document.getElementById('inputLastNameE').value
-                    dataToPocht.age = document.getElementById('inputAgeE').value
-                    dataToPocht.position = document.getElementById('inputPositionE').value
-
-                
-
-                })
-
-
-        }
-        
-        else {
-            console.warn('No se pudo ejecutar')
-        }
-    }
+    
+    fetch(`https://sessiones-js-default-rtdb.firebaseio.com/koders/${hashKoder}/.json`, {
+        method: 'PUT',
+        body: JSON.stringify(dataToPut)
+    }).then(res => {
+        printSingleKoder()
+    }).catch((error) => {
+        console.error('Error:', error);
+      });
 })
 
-
-editKoder.send()
+// ? //////////////////////////////////////////////////////////////////////////////
